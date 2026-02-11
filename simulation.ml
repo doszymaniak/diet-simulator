4013714446a97fc3945ad75ad3903ad1d03955c7
doszymaniak <dominikaszymaniak06@gmail.com>
2026-02-11 11:50:38 +0100

    Add daily_weight_change helper function

--- simulation.ml
+++ simulation.ml
@@ -6,6 +6,15 @@ module Simulation = struct
   let kcal_per_kg = 7700.
 
 
+  let daily_weight_change goal intake special =
+    let diff =
+      match goal with
+      | Maintain -> 0.
+      | Reduce _ -> -500.
+      | Gain _ -> 500.
+    in diff +. special
+
+
   let rec print_weight_change weight daily_diff days curr_days =
     if curr_days > days then weight
     else
@@ -17,11 +26,7 @@ module Simulation = struct
 
   let simulate_weight user =
     let days = Utils.read_int_safe "Enter days to simulate: " in
-    let daily_diff =
-      begin match user.goal with
-      | Maintain -> 0.
-      | Reduce _ -> -500.
-      | Gain _ -> 500. end
+    let daily_diff = daily_weight_change user.goal user.calorie_intake 0.
     in ignore (print_weight_change user.weight daily_diff days 1)
 
 
@@ -31,15 +36,10 @@ module Simulation = struct
     let day = Utils.read_int_safe "Enter the day number: " in
     if day <= 0 || day > days then print_endline "Invalid day number!"
     else
-      let special_calories = Utils.read_int_safe "Enter the number of calories you would like to eat on this day: " in
-      let daily_diff =
-        begin match user.goal with
-        | Maintain -> 0.
-        | Reduce _ -> -500.
-        | Gain _ -> 500. end
-      in 
+      let special_calories = Utils.read_float_safe "Enter the number of calories you would like to eat on this day: " in
+      let daily_diff = daily_weight_change user.goal user.calorie_intake 0. in
+      let special_diff = daily_weight_change user.goal user.calorie_intake special_calories in
       let weight_before = print_weight_change user.weight daily_diff (day - 1) 1 in
-      let special_day_diff = float_of_int (special_calories - user.calorie_intake) in
-      let after_special = print_weight_change weight_before special_day_diff day day in
+      let after_special = print_weight_change weight_before special_diff day day in
       ignore (print_weight_change after_special daily_diff days (day + 1))
 end
