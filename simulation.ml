a6a260063935916662d62abf4c13be58d37faeec
doszymaniak <dominikaszymaniak06@gmail.com>
2026-02-11 12:57:03 +0100

    Add weight safety checks in weight simulation

--- simulation.ml
+++ simulation.ml
@@ -6,13 +6,13 @@ module Simulation = struct
   let kcal_per_kg = 7700.
 
 
-  let daily_weight_change goal intake special =
+  let daily_weight_change goal =
     let diff =
       match goal with
       | Maintain -> 0.
       | Reduce _ -> -500.
       | Gain _ -> 500.
-    in diff +. special
+    in diff
 
 
   let rec print_weight_change weight daily_diff days curr_days =
@@ -20,13 +20,18 @@ module Simulation = struct
     else
       let daily_change = daily_diff /. kcal_per_kg in
       let weight = weight +. daily_change in
-      Printf.printf "Day %d: weight = %.2f kg\n" curr_days weight;
-      print_weight_change weight daily_diff days (curr_days + 1)
+      if weight < 35. then
+        (Printf.printf "Simulation stopped! Weight dangerously low!\n"; weight)
+      else if weight > 300. then
+        (Printf.printf "Simulation stopped! Weight dangerously high!\n"; weight)
+      else
+        (Printf.printf "Day %d: weight = %.2f kg\n" curr_days weight;
+        print_weight_change weight daily_diff days (curr_days + 1))
 
 
   let simulate_weight user =
     let days = Utils.read_int_safe "Enter days to simulate: " in
-    let daily_diff = daily_weight_change user.goal user.calorie_intake 0.
+    let daily_diff = daily_weight_change user.goal
     in ignore (print_weight_change user.weight daily_diff days 1)
 
 
@@ -37,8 +42,8 @@ module Simulation = struct
     if day <= 0 || day > days then print_endline "Invalid day number!"
     else
       let special_calories = Utils.read_float_safe "Enter the number of calories you would like to eat on this day: " in
-      let daily_diff = daily_weight_change user.goal user.calorie_intake 0. in
-      let special_diff = daily_weight_change user.goal user.calorie_intake special_calories in
+      let daily_diff = daily_weight_change user.goal in
+      let special_diff = special_calories -. float_of_int user.calorie_intake in
       let weight_before = print_weight_change user.weight daily_diff (day - 1) 1 in
       let after_special = print_weight_change weight_before special_diff day day in
       ignore (print_weight_change after_special daily_diff days (day + 1))
